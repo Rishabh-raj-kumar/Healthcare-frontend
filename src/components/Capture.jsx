b8c5aec8e374fddaf5ff2b7d12639cb005189500
Rishabh <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2024-03-02 18:16:29 +0530

    updated camera

--- src/components/Capture.jsx
+++ src/components/Capture.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react";
 import Webcam from "react-webcam";
 
 const WebcamComponent = () => <Webcam />;
@@ -6,36 +6,43 @@ const WebcamComponent = () => <Webcam />;
 const videoConstraints = {
   width: 300,
   height: 300,
-  facingMode: "user"
+  facingMode: "user",
 };
 function Capture() {
-    const webcamRef = React.useRef(null);
-    const [image,setImage]=useState('');
-    const capture = React.useCallback(
-        () => {
-          const imageSrc = webcamRef.current.getScreenshot();
-        },
-    
-        [webcamRef]
-      );
+  const webcamRef = React.useRef(null);
+  const [image, setImage] = useState("");
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImage(imageSrc);
+  }, [webcamRef]);
 
   return (
     <div className="webcam-container grid place-items-center h-screen">
-        <div>
-      <Webcam
-        audio={false}
-        height={300}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width={400}
-        videoConstraints={videoConstraints}
-      />
-      <button className=' bg-black px-4 py-3 text-white m-4 rounded'
-      onClick={(e)=>{e.preventDefault();capture();}}>
-      Capture</button>
+      <div>
+        {image == "" ? (
+          <Webcam
+            audio={false}
+            height={300}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={300}
+            videoConstraints={videoConstraints}
+          />
+        ) : (
+          <img src={image} />
+        )}
+        <button
+          className=" bg-black px-4 py-3 text-white m-4 rounded"
+          onClick={(e) => {
+            e.preventDefault();
+            capture();
+          }}
+        >
+          Capture
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Capture
+export default Capture;
